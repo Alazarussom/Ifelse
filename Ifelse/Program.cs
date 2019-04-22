dd9d58961e6c31b373110148a608b67d527cda00
Alazarussom <49820916+Alazarussom@users.noreply.github.com>
2019-04-22 15:33:25 -0400

    initial

---
+++ Ifelse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 10;
+
+
+
+            if (X < 0)
+            {
+                Console.WriteLine("It is a Negative Number");
+
+            }
+            else
+                Console.WriteLine("it is a positive Number");
+
+            Console.ReadLine();
+        }
+    }
+}
